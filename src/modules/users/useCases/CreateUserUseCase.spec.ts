56d0aec8f2ca9bac499d702c3fb5c90bee6b1476
Torres-ssf <sergio.uft@gmail.com>
2020-12-09 08:50:04 -0300

    test(create.user.use.case): make sure execute method create new user

--- src/modules/users/useCases/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/CreateUserUseCase.spec.ts
@@ -1,6 +1,7 @@
 import { v4 } from 'uuid';
 import AppError from '../../../shared/errors/AppError';
 import { User } from '../entities/User';
+import { FakeHashProvider } from '../providers/HashProvider/fakes/FakeHashProvider';
 import { FakeUsersRepository } from '../repositories/fakes/FakeUsersRepository';
 import { CreateUserUseCase } from './CreateUserUseCase';
 
@@ -9,10 +10,19 @@ describe('CreateUserUseCase', () => {
 
   let fakeUsersRepository: FakeUsersRepository;
 
+  let fakeHashProvider: FakeHashProvider;
+
   beforeEach(() => {
+    jest.clearAllMocks();
+
     fakeUsersRepository = new FakeUsersRepository();
 
-    createUserUseCase = new CreateUserUseCase(fakeUsersRepository);
+    fakeHashProvider = new FakeHashProvider();
+
+    createUserUseCase = new CreateUserUseCase(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
   });
 
   it('should not be able to create a new user with an email already registered', async () => {
@@ -33,5 +43,19 @@ describe('CreateUserUseCase', () => {
         password: '123456',
       }),
     ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to create a new user', async () => {
+    const email = 'paul@email.com';
+
+    const createdUser = await createUserUseCase.execute({
+      name: 'Paul Airon',
+      email,
+      password: '123456',
+    });
+
+    const foundedUser = await fakeUsersRepository.findByEmail(email);
+
+    expect(foundedUser).toMatchObject(createdUser);
   });
 });
