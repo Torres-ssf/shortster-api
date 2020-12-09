db409411bab8e0b1b22f4d59c5c85bc9b0bc3c2a
Torres-ssf <sergio.uft@gmail.com>
2020-12-09 17:35:02 -0300

    feat(create.user.controller): handler to create user

---
+++ src/modules/users/useCases/CreateUserController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+export class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body.createUserDTO;
+
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+
+    const user = await createUserUseCase.execute({ name, email, password });
+
+    return response.json(user);
+  }
+}
