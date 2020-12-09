573487e6963b20d7e64c7c4378848e2461ebdbf5
Torres-ssf <sergio.uft@gmail.com>
2020-12-09 17:30:40 -0300

    refactor(create.user.use.case): add tsyringe decorators to properly inject dependecies singletons

--- src/modules/users/useCases/CreateUserUseCase.ts
+++ src/modules/users/useCases/CreateUserUseCase.ts
@@ -1,13 +1,17 @@
+import { inject, injectable } from 'tsyringe';
 import { v4 } from 'uuid';
-import AppError from '../../../shared/errors/AppError';
+import { AppError } from '../../../shared/errors/AppError';
 import { User } from '../entities/User';
 import { IHashProvider } from '../providers/HashProvider/models/IHashProvider';
 import { IUsersRepository } from '../repositories/IUsersRepository';
 import { CreateUserDTO } from './CreateUserDTO';
 
+@injectable()
 export class CreateUserUseCase {
   constructor(
+    @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    @inject('HashProvider')
     private hashProvider: IHashProvider,
   ) {}
 
