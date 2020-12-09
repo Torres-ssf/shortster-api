2ebf9877309b5105dd67bb606fd903b4585cf31c
Torres-ssf <sergio.uft@gmail.com>
2020-12-09 08:49:22 -0300

    feat(create.user.use.case): create user at execute method

--- src/modules/users/useCases/CreateUserUseCase.ts
+++ src/modules/users/useCases/CreateUserUseCase.ts
@@ -1,18 +1,45 @@
+import { v4 } from 'uuid';
 import AppError from '../../../shared/errors/AppError';
 import { User } from '../entities/User';
+import { IHashProvider } from '../providers/HashProvider/models/IHashProvider';
 import { UsersRepository } from '../repositories/UsersRepository';
 import { CreateUserDTO } from './CreateUserDTO';
 
 export class CreateUserUseCase {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(
+    private usersRepository: UsersRepository,
+    private hashProvider: IHashProvider,
+  ) {}
 
   async execute(createUser: CreateUserDTO): Promise<User> {
-    const { email } = createUser;
+    const { email, password } = createUser;
 
-    const userExists = this.usersRepository.findByEmail(email);
+    const userExists = await this.usersRepository.findByEmail(email);
 
     if (userExists) {
       throw new AppError('Email already taken');
+    }
+
+    const salt = await this.hashProvider.generateSalt();
+
+    const hashedPassword = await this.hashProvider.generateHash({
+      payload: password,
+      salt,
+    });
+
+    const user = new User();
+
+    Object.assign(user, {
+      id: v4(),
+      ...createUser,
+      password: hashedPassword,
+      salt,
+    });
+
+    try {
+      return this.usersRepository.save(user);
+    } catch (err) {
+      throw new AppError('Error occurred while trying to create new user.');
     }
 
     return new User();
