e566eb3dc5f2191cce181c0f532f348cbd348a43
Torres-ssf <sergio.uft@gmail.com>
2020-12-09 12:05:20 -0300

    feat(singleton container): create file at users provider to register all providers singletons

---
+++ src/modules/users/providers/index.ts
@@ -0,0 +1,6 @@
+import { container } from 'tsyringe';
+
+import { IHashProvider } from './HashProvider/models/IHashProvider';
+import { FakeHashProvider } from './HashProvider/fakes/FakeHashProvider';
+
+container.registerSingleton<IHashProvider>('UsersRepository', FakeHashProvider);
