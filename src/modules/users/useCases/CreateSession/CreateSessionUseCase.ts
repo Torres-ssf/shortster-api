eb2c807868830c1a353c4bff6dc8bec03abf5e6d
Torres-ssf <sergio.uft@gmail.com>
2020-12-10 11:54:43 -0300

    feat(create.session.use.case): implement functionality to properly sign in an user

--- src/modules/users/useCases/CreateSession/CreateSessionUseCase.ts
+++ src/modules/users/useCases/CreateSession/CreateSessionUseCase.ts
@@ -1,8 +1,16 @@
 import { inject, injectable } from 'tsyringe';
+import { sign } from 'jsonwebtoken';
 import { AppError } from '@shared/errors/AppError';
+import { auth } from '@config/auth';
+import { User } from '@modules/users/entities/User';
 import { IHashProvider } from '../../providers/HashProvider/models/IHashProvider';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 import { CreateSessionDTO } from './CreateSessionDTO';
+
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 @injectable()
 export class CreateSessionUseCase {
@@ -13,22 +21,31 @@ export class CreateSessionUseCase {
     private hashProvider: IHashProvider,
   ) {}
 
-  async execute(createSession: CreateSessionDTO): Promise<void> {
+  async execute(createSession: CreateSessionDTO): Promise<IResponse> {
     const { email, password } = createSession;
 
-    const userExists = await this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email);
 
-    if (!userExists) {
+    if (!user) {
       throw new AppError('wrong email/password combination', 403);
     }
 
     const passwordMatch = await this.hashProvider.compare({
       payload: password,
-      hashed: userExists.password,
+      hashed: user.password,
     });
 
     if (!passwordMatch) {
       throw new AppError('wrong email/password combination', 403);
     }
+
+    const { secret, expiresIn } = auth.jwt;
+
+    const token = sign({}, secret as string, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
   }
 }
