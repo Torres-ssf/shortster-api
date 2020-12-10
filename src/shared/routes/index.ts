312b157221e0a19f620359b16c56f7c8acfac0dd
Torres-ssf <sergio.uft@gmail.com>
2020-12-10 13:24:36 -0300

    feat(app routes): add sessions routes to app routes file

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
 import { usersRoutes } from '../../modules/users/routes/users.routes';
+import { sessionsRoutes } from '../../modules/users/routes/sessions.routes';
 
 export const appRoutes = Router();
 
 appRoutes.use('/users', usersRoutes);
+appRoutes.use('/session', sessionsRoutes);
