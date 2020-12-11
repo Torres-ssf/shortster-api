1bbc6b431d2f8362b5a3b7bbebd96ff669b10a7d
Torres-ssf <sergio.uft@gmail.com>
2020-12-11 18:05:28 -0300

    feat(shortste.routes): add get route to shortster routes

--- src/modules/shortsters/routes/shortster.routes.ts
+++ src/modules/shortsters/routes/shortster.routes.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import { CreateShortsterController } from '../useCases/CreateShortster/CreateShortsterController';
 import { createShortsterMiddleware } from '../useCases/CreateShortster/createShortsterMiddleware';
 import { checkAuthenticated } from '../middlewares/checkUserAuthenticated';
+import { GetShortsterController } from '../useCases/GetShortster/GetShortsterController';
 
 export const shortsterRoutes = Router();
 
 const createShortsterController = new CreateShortsterController();
+
+const getShortsterController = new GetShortsterController();
 
 shortsterRoutes.post(
   '/',
@@ -13,3 +16,5 @@ shortsterRoutes.post(
   createShortsterMiddleware,
   createShortsterController.handle,
 );
+
+shortsterRoutes.get('/:code', getShortsterController.handle);
