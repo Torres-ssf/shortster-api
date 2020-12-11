e0a491ad8478eb11b8e1bae6fe6ef0fde6cc3ff6
Torres-ssf <sergio.uft@gmail.com>
2020-12-11 08:47:48 -0300

    feat(routes app): add shortster routes file to the app main routes

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
-import { usersRoutes } from '../../modules/users/routes/users.routes';
-import { sessionsRoutes } from '../../modules/users/routes/sessions.routes';
+import { shortsterRoutes } from '@modules/shortsters/routes/shortster.routes';
+import { usersRoutes } from '@modules/users/routes/users.routes';
+import { sessionsRoutes } from '@modules/users/routes/sessions.routes';
 
 export const appRoutes = Router();
 
 appRoutes.use('/users', usersRoutes);
+
 appRoutes.use('/session', sessionsRoutes);
+
+appRoutes.use('/shortster', shortsterRoutes);
