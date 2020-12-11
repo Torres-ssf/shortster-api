7ebaa3fafea02600f60859201e0c5a0859f3b109
Torres-ssf <sergio.uft@gmail.com>
2020-12-11 14:45:11 -0300

    test(get.shortster): ensure times_acessed property is increase by one when a shortster is get

--- src/modules/shortsters/useCases/GetShortster/GetShortsterUseCase.spec.ts
+++ src/modules/shortsters/useCases/GetShortster/GetShortsterUseCase.spec.ts
@@ -1,19 +1,33 @@
 import 'reflect-metadata';
 
-import { Shortster } from '@modules/shortsters/entities/Shortster';
 import { FakeShortstersRepository } from '@modules/shortsters/repositories/fakes/FakeShortsterRepository';
 
+import { FakeUsersRepository } from '@modules/users/repositories/fakes/FakeUsersRepository';
 import { GetShortsterUseCase } from './GetShortsterUseCase';
+import { CreateShortsterUseCase } from '../CreateShortster/CreateShortsterUseCase';
 
 describe('GetShortsterUseCase', () => {
   let getShortsterUseCase: GetShortsterUseCase;
 
+  let createShortsterUseCase: CreateShortsterUseCase;
+
+  let fakeUsersRepository: FakeUsersRepository;
+
   let fakeShortstersRepository: FakeShortstersRepository;
 
+  const googleUrl = 'https://www.google.com';
+
   beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
     fakeShortstersRepository = new FakeShortstersRepository();
 
     getShortsterUseCase = new GetShortsterUseCase(fakeShortstersRepository);
+
+    createShortsterUseCase = new CreateShortsterUseCase(
+      fakeUsersRepository,
+      fakeShortstersRepository,
+    );
   });
 
   it('should return an error if no shortster is found for the given code', async () => {
@@ -22,5 +36,15 @@ describe('GetShortsterUseCase', () => {
     ).rejects.toMatchObject({
       message: 'no shortster found for the given id',
     });
+  });
+
+  it('should increase by one shortster times_accessed property every time a shortster is accessed', async () => {
+    const { code, times_accessed } = await createShortsterUseCase.execute({
+      url: googleUrl,
+    });
+
+    const updatedShortester = await getShortsterUseCase.execute(code);
+
+    expect(updatedShortester.times_accessed).toBe(times_accessed + 1);
   });
 });
