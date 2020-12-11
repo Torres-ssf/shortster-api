fa5e7ec0e931308c6c11b89a3ac647427097f370
Torres-ssf <sergio.uft@gmail.com>
2020-12-11 10:59:54 -0300

    feat(shortster.routes): add checkUserAuthenticated middleware to shortster create route

--- src/modules/shortsters/routes/shortster.routes.ts
+++ src/modules/shortsters/routes/shortster.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { CreateShortsterController } from '../useCases/CreateShortster/CreateShortsterController';
 import { createShortsterMiddleware } from '../useCases/CreateShortster/createShortsterMiddleware';
+import { checkAuthenticated } from '../middlewares/checkUserAuthenticated';
 
 export const shortsterRoutes = Router();
 
@@ -8,6 +9,7 @@ const createShortsterController = new CreateShortsterController();
 
 shortsterRoutes.post(
   '/',
+  checkAuthenticated,
   createShortsterMiddleware,
   createShortsterController.handle,
 );
