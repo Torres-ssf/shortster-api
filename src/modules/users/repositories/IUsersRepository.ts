9b9b6a9109274a2693f769853df780fbdc17ca3c
Torres-ssf <sergio.uft@gmail.com>
2020-12-12 14:39:39 -0300

    refactor(iuser.repo): add create method to IUserRepository

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,6 +1,7 @@
 import { User } from '../entities/User';
 
 export interface IUsersRepository {
+  create(createUserDTO: CreateUserDTO): Promise<User>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   save(user: User): Promise<User>;
